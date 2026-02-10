b2a521a42530b5700456b5df5ade19e6da5f4aa3
Alebro112 <sevas.bl@gmail.com>
2026-02-10 19:59:45 +0200

    feat: centralize grpc client registrtion

---
+++ lib/grpc/registry/grpc.registry.ts
@@ -0,0 +1,25 @@
+import { PROTO_PATH } from '@alebooking/contracts'
+
+export interface GrpcClientOptions {
+	package: string
+	protoPath: string
+	env: string
+}
+
+export const GRPC_CLIENTS = {
+	AUTH_PACKAGE: {
+		package: 'auth.v1',
+		protoPath: PROTO_PATH.AUTH,
+		env: 'AUTH_GRPC_URL'
+	},
+	ACCOUNT_PACKAGE: {
+		package: 'account.v1',
+		protoPath: PROTO_PATH.ACCOUNT,
+		env: 'AUTH_GRPC_URL'
+	},
+	USER_PACKAGE: {
+		package: 'user.v1',
+		protoPath: PROTO_PATH.USER,
+		env: 'USER_GRPC_URL'
+	}
+} as const satisfies Record<string, GrpcClientOptions>

e82c9070625c57fae8f35a050cfd76bbce1b844d
Alebro112 <sevas.bl@gmail.com>
2026-02-22 18:23:17 +0200

    feat: timestamp-to-iso helper

--- lib/utils/timestamp-to-iso.ts
+++ lib/utils/timestamp-to-iso.ts
@@ -1,6 +1,3 @@
-import { Timestamp } from "@grpc/grpc-js/build/src/generated/google/protobuf/Timestamp";
-
-
-export function timestampToISO(ts: Timestamp): string | null {
-    return new Date(Number(ts.seconds) * 1000 + ts.nanos / 1_000_000).toISOString()
+export function timestampToISO(ts: {seconds: number, nanos: number}): string | null {
+    return new Date(ts.seconds * 1000 + ts.nanos / 1_000_000).toISOString()
 }
